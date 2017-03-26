6e4b7638cbadcff29111b3fffd431fd943b450be
pambisito <carlos_2017_1@hotmail.com>
2017-03-26 14:36:17 -0500

    Update and rename 282A to 282A - Bit++.cpp

---
+++ 282A - Bit++.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int n; cin>>n; 
+	suma = 0;
+	string s;
+	
+	for( int i = 1 ; i <= n ; i++ ) {
+		cin>>s;
+		
+		if( s == "++X" ) { suma++; }
+		if( s == "X++" ) { suma++; }
+		if( s == "--X" ) { suma--; }
+		if( s == "X--" ) { suma--; }	
+	}
+
+	cout<<suma;
+	
+	return 0;
+}
