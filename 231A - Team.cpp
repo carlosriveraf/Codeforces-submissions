c74d0623db0c75efc631bec8a73d79db06ce5eb4
pambisito <carlos_2017_1@hotmail.com>
2017-03-26 01:22:04 -0500

    Update and rename 231A - Team to 231A - Team.cpp

--- 231A - Team.cpp
+++ 231A - Team.cpp
@@ -5,9 +5,9 @@ int main(){
 	int n, P, V, T, problemas = 0;
 	cin>>n;
 
-	for( int i=1 ; i<=n ; ++i ){
+	for( int i = 1 ; i <= n ; ++i ) {
 		cin>>P; cin>>V; cin>>T;	
-		if( P+V+T >= 2 ){problemas++;}	
+		if( P + V + T >= 2 ) { problemas++; }	
 	}
   
 	cout<<problemas;
