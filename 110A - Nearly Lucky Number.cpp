2d925418fd1a7638f63be71d31d705633c0f3912
pambisito <carlos_2017_1@hotmail.com>
2017-03-26 01:16:56 -0500

    Update 110A - Nearly Lucky Number.cpp

--- 110A - Nearly Lucky Number.cpp
+++ 110A - Nearly Lucky Number.cpp
@@ -4,14 +4,14 @@ using namespace std;
 int main(){
 	string s;
 	cin>>s;
-	int count=0;
+	int count = 0;
 	
-	for(int i=0;i<s.length();i++){
-		if(s[i]=='4'||s[i]=='7'){count++;}	
+	for( int i = 0 ; i < s.length() ; i++ ) {
+		if( s[i] == '4' || s[i] == '7' ){ count++; }	
 	}
 	
-	if(count==4||count==7){cout<<"YES";}
-	else{cout<<"NO";}
+	if( count == 4 || count == 7 ) { cout<<"YES"; }
+	else{ cout<<"NO"; }
 	
 	return 0;
 }
