c78b6cbcfad4f668cbbd7611e5b09eaa935ef781
pambisito <carlos_2017_1@hotmail.com>
2017-03-26 14:38:42 -0500

    Update and rename 467A to 467A - George and Accommodation.cpp

--- 467A - George and Accommodation.cpp
+++ 467A - George and Accommodation.cpp
@@ -3,12 +3,12 @@ using namespace std;
 
 int main(){
 	int n; cin>>n;
-	int p,q;
-	int count=0;
+	int p, q;
+	int count = 0;
 	
-	for(int i=1;i<=n;i++){
+	for( int i = 1 ; i <= n ; i++ ) {
 		cin>>p>>q;
-		if((q-p)>=2){count++;}
+		if( ( q - p ) >= 2 ) { count++; }
 	}
 	
 	cout<<count;
