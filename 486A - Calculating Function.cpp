5b6dfd8644a196749234d15384e0f489d48b241b
pambisito <carlos_2017_1@hotmail.com>
2017-03-26 14:45:17 -0500

    Update and rename 486A to 486A - Calculating Function.cpp

--- 486A - Calculating Function.cpp
+++ 486A - Calculating Function.cpp
@@ -3,12 +3,11 @@
 using namespace std;
 
 int main(){
-	
 	long long x;
 	cin>>x;
 	
-	if(x%2==0){x/=2;cout<<x;}
-	else{x=((x+1)/2)*(-1);cout<<x;}
+	if( x % 2 == 0 ) { x/=2; cout<<x; }
+	else{ x = ( ( x + 1 ) / 2 ) * (-1); cout<<x; }
 	
 	return 0;
 }
