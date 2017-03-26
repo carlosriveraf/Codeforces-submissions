82a4f120e69ef64f33a0b6d60db20db95121e810
pambisito <carlos_2017_1@hotmail.com>
2017-03-26 01:20:55 -0500

    Update and rename 1A - Theatre Square to 1A - Theatre Square.cpp

--- 1A - Theatre Square.cpp
+++ 1A - Theatre Square.cpp
@@ -7,11 +7,11 @@ int main(){
 	long long a; cin>>a;
 	long long x, y;
 	
-	if(m%a!=0){x=(m/a)+1;}
-	else{x=m/a;}
+	if( m % a != 0 ) { x = ( m / a ) + 1; }
+	else{ x = m / a; }
 	
-	if(n%a!=0){y=(n/a)+1;}
-	else{y=n/a;}
+	if( n % a != 0 ) { y = ( n / a ) + 1; }
+	else{ y = n / a; }
 	
 	cout<<x*y;
 	
