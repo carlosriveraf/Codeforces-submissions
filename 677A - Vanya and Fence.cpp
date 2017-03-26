b8c34bc9cd36f60956d2c9fe20eb2ed32dc0ad8e
pambisito <carlos_2017_1@hotmail.com>
2017-03-26 14:58:41 -0500

    Update and rename 677A to 677A - Vanya and Fence.cpp

--- 677A - Vanya and Fence.cpp
+++ 677A - Vanya and Fence.cpp
@@ -4,14 +4,14 @@ using namespace std;
 int main(){
 	int n; cin>>n;
 	int h; cin>>h;
-	int longitud=0;
+	int longitud = 0;
 	int a;
 	
-	for(int i=1;i<=n;i++){
+	for( int i = 1 ; i <= n ; i++ ) {
 		cin>>a;
 		
-		if(a<=h){longitud++;}
-		else{longitud+=2;}	
+		if( a <= h ) { longitud++; }
+		else{ longitud += 2; }	
 	}
 	
 	cout<<longitud;
