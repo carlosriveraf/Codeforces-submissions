dfba84c574d324d68cd80ef5aa52393c14e140fb
pambisito <carlos_2017_1@hotmail.com>
2017-05-10 22:23:16 -0500

    Create 271A - Beautiful Year.cpp

---
+++ 271A - Beautiful Year.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int year( int a ) {
+	set<int> b;
+	int a1 = a;
+	int c;
+	while( a1 != 0 ) {
+		c = a1 % 10;
+		b.insert(c);
+		a1 /= 10;
+	}
+	
+	if( b.size() == 4 ) return a;
+	else return year(a+1);
+}
+
+int main() {
+	int y;
+	scanf( "%d", &y );
+	
+	int x = year( y + 1 );
+	printf( "%d", x );
+	
+	return 0;
+}
