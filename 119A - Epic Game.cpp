84121bab52d237b627b86922c0a20e7fc6f47b35
pambisito <carlos_2017_1@hotmail.com>
2017-05-11 22:52:59 -0500

    Create 119A - Epic Game.cpp

---
+++ 119A - Epic Game.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int GCD( int a, int b ) {
+     if( b == 0 ) return a;
+     return GCD ( b, a % b );
+}
+
+
+int main() {	
+	int a, b, n;
+	scanf( "%d %d %d", &a, &b, &n );
+	
+	bool c = true;
+	while( n != 0 ) {
+		if( c ) {
+			n = n - GCD( a, n );
+			c = false;
+		}
+		else{
+			n = n - GCD( b, n );
+			c = true;
+		}
+	}
+	
+	if( c ) cout<<1;
+	else cout<<0;
+		
+	return 0;
+}
