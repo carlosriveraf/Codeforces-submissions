5ab2e6436a03b5b62a2143f06d68a7ba215451c0
pambisito <carlos_2017_1@hotmail.com>
2017-06-16 23:09:33 -0500

    Create 59A - Word.cpp

---
+++ 59A - Word.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	string s; cin >> s;
+	
+	int upper = 0, lower = 0;
+	for( int i = 0 ; i < s.size() ; i++ ) {
+		if( 'a' <= s[i] && s[i] <= 'z'  ) lower++;
+		else upper++;		
+	}
+	
+	if( upper < lower ) 
+		for( int i = 0 ; i < s.size() ; i++ ) s[i] = tolower(s[i]);
+	else if( upper > lower )  
+		for( int i = 0 ; i < s.size() ; i++ ) s[i] = toupper(s[i]);
+	else
+		for( int i = 0 ; i < s.size() ; i++ ) s[i] = tolower(s[i]);
+	
+	cout << s;
+	
+	return 0;
+}
