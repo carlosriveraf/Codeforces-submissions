bd769b849028448a4920ef7cd955d5599b2da30a
pambisito <carlos_2017_1@hotmail.com>
2017-06-16 22:44:43 -0500

    Update 711A - Bus to Udayland.cpp

--- 711A - Bus to Udayland.cpp
+++ 711A - Bus to Udayland.cpp
@@ -3,8 +3,8 @@ using namespace std;
 
 int main() {
 	int n; cin >> n;
-	char bus[n][5];
 	
+	char bus[n][5];
 	for( int i = 0 ; i < n ; i++ ) {
 		for( int j = 0 ; j < 5 ; j++ ) {
 			cin >> bus[i][j];
@@ -13,12 +13,13 @@ int main() {
 	
 	bool aux = false;
 	for( int i = 0 ; i < n ; i++ ) {
-		for( int j = 1 ; j < 5 ; j++ ) {
-			if( bus[i][j] == 'O' && bus[i][j-1] == bus[i][j] ) {
-				bus[i][j] = bus[i][j-1] = '+';
-				aux = true;
-			}
-			if( aux ) break;
+		if( bus[i][1] == 'O' && bus[i][0] == bus[i][1] ) {
+			bus[i][1] = bus[i][0] = '+';
+			aux = true;
+		}
+		else if( bus[i][4] == 'O' && bus[i][3] == bus[i][4] ) {
+			bus[i][4] = bus[i][3] = '+';
+			aux = true;			
 		}
 		if( aux ) break;
 	}
